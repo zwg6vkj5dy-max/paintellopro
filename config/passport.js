5f600734528957f1d3ce58e941ff1e2eacbee0a8
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-10-27 07:55:28 +0100

    Refactor passport strategies and user handling

--- config/passport.js
+++ config/passport.js
@@ -1,145 +1,59 @@
-var passport = require('passport');
-var User = require('../models/user');
-var LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
 
-passport.use(new FacebookStrategy({
-  clientID: process.env.FACEBOOK_APP_ID,
-  clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "https://www.paintello.uk/auth/facebook/callback",
-  profileFields: ['id', 'emails', 'name'] // Request email, first and last name
-}, async function(accessToken, refreshToken, profile, done) {
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
   try {
-    const existingUser = await User.findOne({ 'facebookId': profile.id });
-
-    if (existingUser) return done(null, existingUser);
-
-    // If not found, create new user
-    const newUser = new User({
-      facebookId: profile.id,
-      email: profile.emails?.[0].value || `user_${profile.id}@facebook.com`,
-      firstName: profile.name.givenName || '',
-      lastName: profile.name.familyName || '',
-      numero: '', // Facebook doesn't provide phone number
-      isAdmin: false
-    });
-
-    await newUser.save();
-    return done(null, newUser);
-  } catch (err) {
-    return done(err, null);
-  }
-}));
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async function(id, done) {
-  try {
-    const user = await User.findById(id).exec();
+    const user = await User.findById(id);
     done(null, user);
   } catch (err) {
-    done(err, null);
+    done(err);
   }
 });
-// Local Signup Strategy
-passport.use(
-  "local-signup",
-  new LocalStrategy(
-    {
-      usernameField: "email",
-      passwordField: "password",
-      passReqToCallback: true,
-    },
-    async function (req, email, password, done) {
-      try {
-        const existingUser = await User.findOne({ email: email.toLowerCase() });
-        if (existingUser) {
-          return done(null, false, req.flash("signupMessage", "L'email existe déjà."));
-        } else {
-          const newUser = new User();
-          newUser.email = email.toLowerCase();
-          newUser.password = newUser.generateHash(password);
-          newUser.firstName = req.body.firstName.toLowerCase();
-          newUser.lastName = req.body.lastName.toLowerCase();
-          newUser.numero = req.body.numero;
-          await newUser.save();
-          return done(null, newUser);
-        }
-      } catch (err) {
-        return done(err);
-      }
-    }
-  )
-);
 
-// Local Signup Strategy
-passport.use(
-  "local-signup",
-  new LocalStrategy(
-    {
-      usernameField: "email",
-      passwordField: "password",
-      passReqToCallback: true,
-    },
-    async function (req, email, password, done) {
-      try {
-        const existingUser = await User.findOne({ email: email.toLowerCase() });
-        if (existingUser) {
-          return done(null, false, req.flash("signupMessage", "L'email existe déjà."));
-        } else {
-          const newUser = new User();
-          newUser.email = email.toLowerCase();
-          newUser.password = newUser.encryptPassword(password);
-          newUser.firstName = req.body.firstName.toLowerCase();
-          newUser.lastName = req.body.lastName.toLowerCase();
-          newUser.numero = req.body.numero;
-          await newUser.save();
-          return done(null, newUser);
-        }
-      } catch (err) {
-        return done(err);
-      }
-    }
-  )
-);
-
-
-// SIGNIN
-passport.use('local-signin', new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
-  passReqToCallback: true
-}, async function(req, email, password, done) {
-  req.assert('email', 'invalid email').notEmpty().isEmail();
-  req.assert('password', 'invalid password (must be more than 4 characters)').notEmpty();
-  var errors = req.validationErrors();
-  if (errors) {
-    var messages = errors.map(error => error.msg);
-    return done(null, false, req.flash('error', messages));
-  }
-
+// Local strategy
+passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
   try {
-    const user = await User.findOne({ email }).exec();
-    if (!user) {
-      return done(null, false, { message: 'No user found.' });
-    }
-
-    if (!user.validPassword(password)) {
-      return done(null, false, { message: 'Wrong password.' });
-    }
-
-    req.session.user = {
-      email: user.email,
-      numero: user.numero,
-      firstName: user.firstName,
-      lastName: user.lastName
-    };
-
+    const user = await User.findOne({ email });
+    if (!user) return done(null, false, { message: 'Invalid credentials' });
+    const ok = await bcrypt.compare(password, user.passwordHash || '');
+    if (!ok) return done(null, false, { message: 'Invalid credentials' });
     return done(null, user);
-  } catch (err) {
-    return done(err);
+  } catch (e) {
+    return done(e);
   }
 }));
+
+// Facebook strategy
+passport.use(new FacebookStrategy({
+  clientID: process.env.FB_ID,
+  clientSecret: process.env.FB_SECRET,
+  callbackURL: process.env.FB_CALLBACK,
+  profileFields: ['id','emails','name']
+}, async (_, __, profile, done) => {
+  try {
+    const email = profile.emails?.[0]?.value;
+    let user = await User.findOne({ email });
+    if (!user) {
+      user = await User.create({
+        role: 'client',
+        name: `${profile.name.givenName} ${profile.name.familyName}`.trim(),
+        email,
+        passwordHash: ''
+      });
+    }
+    done(null, user);
+  } catch (e) {
+    done(e);
+  }
+}));
+
+// Optional Google strategy scaffold
+// passport.use(new GoogleStrategy({ ... }, async (accessToken, refreshToken, profile, done) => { ... }));
+
+module.exports = passport;
 
