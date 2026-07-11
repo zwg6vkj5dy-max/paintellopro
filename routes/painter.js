621eefebd769a572adde56cc17eeb5ebe321a803
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2026-07-11 02:55:58 +0200

    Add imports for metaCapi and userData utilities

--- routes/painter.js
+++ routes/painter.js
@@ -4,6 +4,9 @@ const Painter = require('../models/Painter');
 const Order = require('../models/Order');
 const wilayas = require('../utils/wilayas');
 const StoreProduct = require('../models/StoreProduct'); // Make sure this is at the top
+const sendMetaCAPIEvent = require('../services/metaCapi');
+const getCleanUserData = require('../utils/userData');
+
 function generateEventId() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
     const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
